3255ec0902d38efb963b6c6e7dbfccaf09f46f8e
User <User@WIN-AK2SGH7E72N>
2020-10-10 21:00:09 +0300

    Добавьте файлы проекта.

---
+++ Prog_Laba1_Task3/Prog_Laba1_Task3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    char* mystrcat(char* to, const char* from);
+    char destStr[40], srcStr[20];
+
+    cout << "Enter destination string: "; cin >> destStr;
+    cout << "Enter source string: "; cin >> srcStr;
+    cout << "First Result: " << mystrcat(destStr, srcStr) << endl;
+
+    cout << "Enter source string: "; cin >> srcStr;
+    mystrcat(destStr, srcStr);
+    cout << "Second result: " << destStr;
+
+}
+
+char* mystrcat(char* str1, const char* str2) {
+    char* begin = str1;
+    while (*str1) str1++;
+
+    while (*str1++ = *str2++);
+
+    *str1 = '\0';
+    return begin;
+}
